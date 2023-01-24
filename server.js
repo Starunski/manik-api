bdf322da9f80eb302668a8ed9fcb55bcf6e0061a
Dmitri Starunski <dmitriy.starunskiy@gmail.com>
2023-01-24 17:29:28 +0100

    add netlify

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const userRouter = require("./routes/user.routes");
 const postRouter = require("./routes/post.routes");
+
 // app.get("/api", (req, res) => {
-//   //   res.send("Hello World!");
 //   res.json({ users: ["tobi", "loki", "jane"] });
 // });
 
@@ -14,3 +15,5 @@ app.use("/api", postRouter);
 app.listen(5000, () => {
   console.log("Example app listening on port 5000!");
 });
+
+// module.exports.handler = serverless(app);
